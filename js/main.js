ea1ebfdc0fcb16fc83ca203085faad5a09a01598
Octavio Zenil <octaviozenil@gmail.com>
2024-01-17 11:56:10 -0700

    Place Queen

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+const QUEEN_CLASS = 'queen'
+const REINAS_RESTANTES = document.getElementById('restantes')
+const REINAS_COLOCADAS = document.getElementById('colocadas')
+
 var queens = 8
 
 document.querySelectorAll('td').forEach(
@@ -10,11 +14,31 @@ function handleClick(event){
     let cell = event.target
     let {row, column} = getCellLocation(cell)
 
+    placeQueenLogic(cell)
 }
 
 function placeQueenLogic(cell){
     if (cell.classList.contains('queen'))
-        quitQueen(cell) 
+        removeQueen(cell)
+    else
+        placeQueen(cell)
+
+    refreshScore(queens)
+    
+}
+
+function placeQueen(cell){
+    if (queens > 8 || queens <= 0)
+        return
+    cell.classList.add(QUEEN_CLASS)
+    queens--
+}
+
+
+function refreshScore(value){
+    REINAS_RESTANTES.innerText = `Reinas restantes: ${value}`
+    REINAS_COLOCADAS.innerText = `Reinas colocadas: ${(8-value)}`
+    
 }
 
 function getCellLocation(cell){
