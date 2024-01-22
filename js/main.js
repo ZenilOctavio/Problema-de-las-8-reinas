ba694270a5d589ff5e639d1859658df002b4e762
Octavio Zenil <octaviozenil@gmail.com>
2024-01-22 10:37:47 -0700

    Removing ocupied cells

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
+// loadScript('js/logger.js')
+
 const QUEEN_CLASS = 'queen'
 const LOCKED_CLASS = 'locked'
 const REINAS_RESTANTES = document.getElementById('restantes')
 const REINAS_COLOCADAS = document.getElementById('colocadas')
 
 
-var lockedCells = new Set()
+// var lockedCells = new Set()
 var queens = 8
 var placedQueens = []
 var cells = document.querySelectorAll('td')
+var ocupiedPDiagonals = []
+var ocupiedSDiagonals = []
+var ocupiedColumns = []
+var ocupiedRows = []
 
 cells.forEach(
     cell => {
         cell.addEventListener('click', handleClick)
-        let {row, column} = getCellLocation(cell)
+        // log('eventListener added')
+        // let {row, column} = getCellLocation(cell)
         // cell.innerText = `(${row},${column})`
     }
 )
@@ -44,8 +51,18 @@ function placeQueen(cell){
     placedQueens.push(location)
     lockQueenMovements(location.row, location.column)
     cell.classList.remove(LOCKED_CLASS)
-    lockedCells.delete(cell)
+    // lockedCells.delete(cell)
+    log_ocupied()
     queens--
+}
+
+function log_ocupied(){
+    console.group('Ocupied')
+    console.log(`Ocupied rows: ${ocupiedRows}`)
+    console.log(`Ocupied columns: ${ocupiedColumns}`)
+    console.log(`Ocupied principal diagonals: ${ocupiedPDiagonals}`)
+    console.log(`Ocupied secondary diagonals: ${ocupiedSDiagonals}`)
+    console.groupEnd()
 }
 
 function removeQueen(cell){
@@ -55,28 +72,31 @@ function removeQueen(cell){
     let index = placedQueens.findIndex((element) => element.row == row && element.column == column)
     placedQueens.splice(index,1)
     unlockQueenMovements(row, column)
+    log_ocupied()
     queens++
 }
 
 function lockQueenMovements(row, column){
-    lockColumns(column)
-    lockRows(row)
+    lockColumn(column)
+    lockRow(row)
     lockPrincipalDiagonal(row, column)
     lockSecondaryDiagonal(row, column)
+
 }
 
-function lockColumns(column){
+function lockColumn(column){
     console.log('Locking columns')
     cells.forEach((cell) => {
-        console.log(cell.cellIndex, column)
+        // console.log(cell.cellIndex, column)
         if (cell.cellIndex == column && !cell.classList.contains(QUEEN_CLASS)){
             lockCell(cell)
 
         }
-})
+    })
+    ocupiedColumns.push(column)
 }
 
-function lockRows(row){
+function lockRow(row){
     console.log('Locking rows')
     cells.forEach((cell) => {
         if (cell.parentElement.rowIndex == row && !cell.classList.contains(QUEEN_CLASS)){
@@ -84,6 +104,8 @@ function lockRows(row){
 
         }
     })
+
+    ocupiedRows.push(row)
 }
 
 function lockPrincipalDiagonal(row,column){
@@ -98,6 +120,7 @@ function lockPrincipalDiagonal(row,column){
         }
 
     })
+    ocupiedPDiagonals.push(difference)
 }
 
 function lockSecondaryDiagonal(row, column){
@@ -112,12 +135,13 @@ function lockSecondaryDiagonal(row, column){
         }
 
     })
+    ocupiedSDiagonals.push(addition)
 }
 
 function lockCell(cell){
     cell.classList.add(LOCKED_CLASS)
-    lockedCells.add(cell)
-    console.log(lockedCells)
+    // lockedCells.add(cell)
+    // console.log(lockedCells)
 }
 
 function unlockQueenMovements(row, column){
@@ -128,6 +152,7 @@ function unlockQueenMovements(row, column){
 }
 
 function unlockColumn(column){
+    removeElementFromArray(column, ocupiedColumns)
     cells.forEach(
         cell => {
             if (cell.cellIndex == column)
@@ -137,6 +162,7 @@ function unlockColumn(column){
 }
 
 function unlockRow(row){
+    removeElementFromArray(row, ocupiedRows)
     cells.forEach(
         cell => {
             if (cell.parentElement.rowIndex== row)
@@ -147,7 +173,7 @@ function unlockRow(row){
 
 function unlockPrincipalDiagonal(row, column){
     let difference = row - column
-
+    removeElementFromArray(difference, ocupiedPDiagonals)
     cells.forEach(
         cell => {
             let currentDifference = cell.parentElement.rowIndex - cell.cellIndex
@@ -159,7 +185,7 @@ function unlockPrincipalDiagonal(row, column){
 
 function unlockSecondaryDiagonal(row, column){
     let addition = row + column
-
+    removeElementFromArray(addition, ocupiedSDiagonals)
     cells.forEach(
         cell => {
             let currentAddition = cell.parentElement.rowIndex + cell.cellIndex
@@ -171,9 +197,17 @@ function unlockSecondaryDiagonal(row, column){
 }
 
 function unlockCell(cell){
+    let {row, column} = getCellLocation(cell)
+    let pDiagonalId = row - column
+    let sDiagonalId = row + column
+
+    if (ocupiedPDiagonals.includes(pDiagonalId))    return
+    if (ocupiedSDiagonals.includes(sDiagonalId))    return
+    if (ocupiedColumns.includes(column))    return
+    if (ocupiedRows.includes(row))  return
+
     cell.classList.remove(LOCKED_CLASS)
-    lockedCells.delete(cell)
-    console.log(lockedCells)
+
 }
 
 function refreshScore(value){
@@ -185,3 +219,16 @@ function refreshScore(value){
 function getCellLocation(cell){
     return {row: cell.parentElement.rowIndex, column: cell.cellIndex}
 }
+
+function removeElementFromArray(value, array){
+    let index = array.findIndex(element => element == value)
+    array.splice(index,1)
+}
+
+// function loadScript(url){
+//     let head = document.getElementsByTagName('head')[0]
+//     let script = document.createElement('script')
+//     script.type = 'text/javascript'
+//     script.src = url
+//     head.appendChild(script)
+// }
