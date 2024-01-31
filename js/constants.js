1f03042a44b6f9a5f498103954eb12f15483e448
Octavio Zenil <octaviozenil@gmail.com>
2024-01-30 19:44:01 -0700

    Key for solutions array in localstorage added

--- js/constants.js
+++ js/constants.js
@@ -3,4 +3,5 @@ const LOCKED_CLASS = 'locked'
 const AVAILABLE_CLASS = 'available'
 const REINAS_RESTANTES = document.getElementById('restantes')
 const REINAS_COLOCADAS = document.getElementById('colocadas')
+const SOLUTIONS_KEY = 'solutions'
 
